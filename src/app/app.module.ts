2b6c4fd00c1222caa015ba914f98330b97c8ee33
Chandan Bharadwaj <Sai.ChandanBharadwajKolluru9@T-Mobile.com>
2017-08-16 16:59:04 +0530

    Init-changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { InitializrComponent } from './initializr/initializr.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitializrComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
+  schemas: [NO_ERRORS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/initializr/initializr.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+declare var Materialize: any;
+declare var $: any;
+@Component({
+  selector: 'app-initializr',
+  templateUrl: './initializr.component.html',
+  styleUrls: ['./initializr.component.css']
+})
+export class InitializrComponent implements OnInit {
+
+  constructor() { }
+
+
+  ngOnInit() {
+  }
+
+}
